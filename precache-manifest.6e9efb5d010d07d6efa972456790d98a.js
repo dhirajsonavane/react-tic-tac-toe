ce69fa918b9df67e708de699a0e490523b372bb1
Sonavane, Dhiraj <Dhiraj.Sonavane@tabcorp.com.au>
2020-04-10 19:47:03 +1000

    Updates

---
+++ precache-manifest.6e9efb5d010d07d6efa972456790d98a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5abd51fe08bd2dfbc6a95dc34aeb56a",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "5513a166e2acc5d730c0",
+    "url": "/react-tic-tac-toe/static/css/main.32bec685.chunk.css"
+  },
+  {
+    "revision": "a35e381408f784b5eb4b",
+    "url": "/react-tic-tac-toe/static/js/2.37205815.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-tic-tac-toe/static/js/2.37205815.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5513a166e2acc5d730c0",
+    "url": "/react-tic-tac-toe/static/js/main.902d4b13.chunk.js"
+  },
+  {
+    "revision": "bab408267c22d98e6389",
+    "url": "/react-tic-tac-toe/static/js/runtime-main.be4bbc4c.js"
+  }
+]);
